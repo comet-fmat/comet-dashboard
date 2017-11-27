2baa14f66d7db55d1a8be64ef182032f9edc675a
Isis Naomi <isisnaomi@gmail.com>
2017-11-27 00:56:43 +0000

    Add Exercise methods

--- app/Exercise.php
+++ app/Exercise.php
@@ -20,15 +20,41 @@ class Exercise extends Model
         );
     }
     public function students(){
+        return $this->hasManyThrough(
+            'App\User',
+            'App\Submissions',
+            'exercise_name',
+            'name',
+            'id',
+            'id'
+        );
 
     }
-    public function awardedPoints($student){
+    public function awardedPoints($exercise, $student){
+
+        $exercise = Exercise::where('id', $exercise)->first();
+        $submission = $exercise->submissions->where('user_id', $student)->get();
+        $awardedPoints = AwardedPoint::where('submission_id', $submission);
+        return $awardedPoints;
+
+    }
+    public function submissionsAverage(){
+        $average = 0;
+        $submissions = $this->submissions->get();
+
+        foreach ($submissions as $submission) {
+            $average = $average + $submission->points;
+        }
+
+        return $average;
 
     }
     public function availablePoints(){
+        return $this->hasOne('App\AvailablePoint');
 
     }
     public function testCases(){
-
+        //TODO
+        return null;
     }
 }
--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Exercise;
 use Illuminate\Http\Request;
 
 class ExerciseController extends Controller
 {
-    //
+    public function index($course){
+        $viewData = [];
+
+        //All course exercises
+        $exercises = $course->exercises->get();
+
+        //Data view reply
+        $viewData =[
+            'exercises' => $exercises
+        ];
+
+        return $viewData;
+    }
+
+    public function display(Exercise $exercise)
+    {
+        $viewData = [];
+
+        //Number of students
+        $numStudents = $exercise->students->count();
+
+        //List of students
+        $students = $exercise->students->get();
+
+        //Number of submissions
+        $numSubmissions = $exercise->submissions->count();
+
+        //Submissions average
+        $submissionsAverage = $exercise->submissionsAverage();
+
+        //Data view reply
+        $viewData = [
+            'numStudents' => $numStudents,
+            'students' => $students,
+            'numSubmissions' => $numSubmissions,
+            'submissionsAverage' => $submissionsAverage
+        ];
+
+
+        return $viewData;
+    }
 }
